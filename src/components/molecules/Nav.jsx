fbd3c74fee4fa9e619b6e6440f65492dbf967336
d-alejandro-salas <daniel_cabj88@hotmail.com>
2024-09-02 19:29:07 -0300

    Mejorando el nav

--- src/components/molecules/Nav.jsx
+++ src/components/molecules/Nav.jsx
@@ -7,32 +7,38 @@ import { ModeButton } from "../atoms/ModeButton";
 export const Nav = () => {
   const location = useLocation();
 
-  return (<nav className="flex w-full bg-black p-2 uppercase text-white justify-between border-b border-white">
-
-      <ul className="flex space-x-4 my-auto">
+  return (
+    <nav className="flex flex-col md:flex-row w-full bg-black p-2 uppercase text-white justify-between border-b border-white">
+      <ul className="flex flex-col md:flex-row md:space-x-4 my-auto space-y-2 md:space-y-0">
         <li>
-          <NavLink 
+          <NavLink
             to="/cp"
             className={({ isActive }) =>
-              location.pathname === '/cp' || location.pathname === '/' ? 'active' : undefined
+              location.pathname === '/cp' || location.pathname === '/' ? 'text-yellow-400' : 'text-white'
             }>
             Capital y Patagonia
           </NavLink>
         </li>
         <li>
-          <NavLink 
-            to="/no">
+          <NavLink
+            to="/no"
+            className={({ isActive }) =>
+              location.pathname === '/no' ? 'text-yellow-400' : 'text-white'
+            }>
             Norte y Este
           </NavLink>
         </li>
         <li>
-          <NavLink 
-            to="/ruta">
+          <NavLink
+            to="/ruta"
+            className={({ isActive }) =>
+              location.pathname === '/ruta' ? 'text-yellow-400' : 'text-white'
+            }>
             Ruta
           </NavLink>
         </li>
       </ul>
-      <div>
+      <div className="flex space-x-2 mt-2 md:mt-0">
         <ModeButton />
         <LoggingButton />
       </div>
